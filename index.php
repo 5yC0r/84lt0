bfe9736b090c0d2c8ae11b08a5421b59ef601c3f
Jesus <jesus.lv.fernandez@gmail.com>
2017-01-22 20:25:48 -0500

    preguntas separadas por clases, solo falta sincronizar con base de datos

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <body>
 	<h1>Titulo de la encuesta</h1>
 	<p>Instrucciones: En cada pregunta darle click a alguna alternativa y ver lo que pasa</p>
-	<div class="pregunta" id="p1">
+	<div class="pregunta-alternativa-simple" id="p1">
 		<div class="enunciado">
 			<label for="">1. ¿Al egresar de la carrera que tan preparado se sintió para ejercer la profesión?</label>
 		</div>
@@ -26,7 +26,7 @@
 			</ul>
 		</div>
 	</div>
-	<div class="pregunta" id="p2">
+	<div class="pregunta-alternativa-simple" id="p2">
 		<div class="enunciado">
 			<label for="">2. ¿Cuál fue su percepción sobre el desempeño de los docentes de la Facultad de Estomatología al egresar?</label>
 		</div>
@@ -46,27 +46,27 @@
 		</div>
 		<div class="grupo-alternativas">
 			<ul>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="a">a) Cirugía Bucal y Maxilofacial.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="b">b) Endodoncia.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="c">c) Medicina y Patología Estomatológica.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="d">d) Odontopediatria.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="e">e) Ortodoncia y Ortopedia Maxilar.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="f">f) Periodoncia e Implantología.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="g">g) Radiología Bucal y Maxilofacial.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="h">h) Rehabilitación Oral.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="i">i) Salud Pública Estomatológica.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="j">j) Salud Familiar y Comunitaria en Odontología.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="k">k) Odontología Forense.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="l">l) Estomatología de Pacientes Especiales.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="m">m) Odontología Restauradora y Estética.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="n">n) Odontogeriatría.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="o">o) Auditoría Odontológica.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="p">p) Administración y Gestión en Estomatología.</label></li>
-				<li><label for="" class="alternativa" data-seleccionado="no" data-pregunta="p3" data-indice="q">q) Ninguna.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="a">a) Cirugía Bucal y Maxilofacial.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="b">b) Endodoncia.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="c">c) Medicina y Patología Estomatológica.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="d">d) Odontopediatria.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="e">e) Ortodoncia y Ortopedia Maxilar.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="f">f) Periodoncia e Implantología.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="g">g) Radiología Bucal y Maxilofacial.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="h">h) Rehabilitación Oral.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="i">i) Salud Pública Estomatológica.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="j">j) Salud Familiar y Comunitaria en Odontología.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="k">k) Odontología Forense.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="l">l) Estomatología de Pacientes Especiales.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="m">m) Odontología Restauradora y Estética.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="n">n) Odontogeriatría.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="o">o) Auditoría Odontológica.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="p">p) Administración y Gestión en Estomatología.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p3" data-indice="q">q) Ninguna.</label></li>
 			</ul>
 		</div>
 	</div>
-	<div class="pregunta" id="p4">
+	<div class="pregunta-alternativa-simple" id="p4">
 		<div class="enunciado">
 			<label for="">4. ¿Usted tiene intención de emigrar</label>
 		</div>
@@ -78,18 +78,18 @@
 			</ul>
 		</div>
 	</div>
-	<div class="pregunta" id="p5">
+	<div class="pregunta-alternativa-simple" id="p5">
 		<div class="enunciado">
 			<label for="">5. ¿Usted volvería a seleccionar la misma carrera?</label>
 		</div>
 		<div class="grupo-alternativas">
 			<ul>
 				<li><label for="" class="alternativa" data-pregunta="p5" data-indice="a">a) Si.</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p5" data-indice="b">b) No.</label></li>
+				<li><label for="" class="alternativa" data-pregunta="p5" data-indice="b">b) false.</label></li>
 			</ul>
 		</div>
 	</div>
-	<div class="pregunta" id="p6">
+	<div class="pregunta-alternativa-simple" id="p6">
 		<div class="enunciado">
 			<label for="">6. ¿Usted volvería a elegir la misma universidad?</label>
 		</div>
@@ -100,7 +100,7 @@
 			</ul>
 		</div>
 	</div>
-	<div class="pregunta" id="p7">
+	<div class="pregunta-compuesta" id="p7">
 		<div class="enunciado">
 			<label for="">7. ¿Usted está titulado?</label>
 		</div>
@@ -111,18 +111,18 @@
 			</ul>
 		</div>
 
-		<div class="pregunta-adicional" id="p7-1">
+		<div class="pregunta-adicional" id="p7-1" data-head="b">
 			<div class="enunciado">
-				<label for="">Explique a continuación el motivo por el cual no se tituló y de por terminada la encuesta.</label>
+				<label for="">Explique a continuación el motivo por el cuál no se tituló y de por terminada la encuesta.</label>
 			</div>
 			<div class="respuesta-tipeada">
 				<input type="text">
 			</div>
 		</div>
 	</div>
-	<div class="pregunta" id="p8">
+	<div class="pregunta-compuesta" id="p8">
 		<div class="enunciado">
-			<label for="">8. Luego de haber egresado, ¿se ha capacitado profesionalmente?</label>
+			<label for="">8. Luego de haber egresado, ¿Se ha capacitado profesionalmente?</label>
 		</div>
 		<div class="grupo-alternativas">
 			<ul>
@@ -130,7 +130,7 @@
 				<li><label for="" class="alternativa" data-pregunta="p8" data-indice="b">b) No.</label></li>
 			</ul>
 		</div>
-		<div class="pregunta-adicional" id="p8-1">
+		<div class="pregunta-adicional" id="p8-1" data-head="a">
 			<div class="enunciado">
 				<label for="">Usted se ha capacidado profesionalmente en: (puede marcar más de una opcion)</label>
 			</div>
@@ -144,7 +144,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="pregunta" id="p9">
+	<div class="pregunta-compuesta" id="p9">
 		<div class="enunciado">
 			<label for="">9. ¿Usted actualmente esta ejerciendo la profesión?</label>
 		</div>
@@ -155,7 +155,7 @@
 			</ul>
 		</div>
 
-		<div class="pregunta-adicional" id="p9-1">
+		<div class="pregunta-adicional" id="p9-1" data-head="b">
 			<div class="enunciado">
 				<label for="">¿Cuál es el motivo por el que no ejerce la profesión?</label>
 			</div>
@@ -170,12 +170,12 @@
 		</div>
 		<div class="grupo-alternativas">
 			<ul>
-				<li><label for="" class="alternativa" data-pregunta="p10" data-indice="a">a) Asistencial.</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p10" data-indice="b">b) Docencia.</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p10" data-indice="c">c) Investigación.</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p10" data-indice="d">d) Odontología legal y forense.</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p10" data-indice="e">e) Administración.</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p10" data-indice="e">f) Promoción de la salud.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p10" data-indice="a">a) Asistencial.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p10" data-indice="b">b) Docencia.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p10" data-indice="c">c) Investigación.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p10" data-indice="d">d) Odontología legal y forense.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p10" data-indice="e">e) Administración.</label></li>
+				<li><label for="" class="alternativa" data-seleccionado="false" data-pregunta="p10" data-indice="e">f) Promoción de la salud.</label></li>
 			</ul>
 		</div>
 	</div>
@@ -190,7 +190,7 @@
 			</ul>
 		</div>
 	</div>
-	<div class="pregunta" id="p12">
+	<div class="pregunta-compuesta" id="p12">
 		<div class="enunciado">
 			<label for="">12. Actualmente su estado laboral como cirujano dentista es:</label>
 		</div>
@@ -198,40 +198,67 @@
 			<ul>
 				<li><label for="" class="alternativa" data-pregunta="p12" data-indice="a">a) Independiente.</label></li>
 				<li><label for="" class="alternativa" data-pregunta="p12" data-indice="b">b) Dependiente.</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p12" data-indice="b">c) Dependiente e independiente.</label></li>
+				<li><label for="" class="alternativa" data-pregunta="p12" data-indice="">c) Dependiente e independiente.</label></li>
 				<li><label for="" class="alternativa" data-pregunta="p12" data-indice="b">d) Desempleado.</label></li>
 			</ul>
 		</div>
-	</div>
-	<div class="pregunta" id="p13">
-		<div class="enunciado">
-			<label for="">13. ¿Como trabajador...?</label>
-		</div>
-		<div class="grupo-alternativas">
-			<ul>
-				<li><label for="" class="alternativa" data-pregunta="p2" data-indice="a">a) Si</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p2" data-indice="b">b) No</label></li>
-			</ul>
+
+		<div class="pregunta-adicional" id="p12-1" data-head="b">
+			<div class="enunciado">
+				<label for="">Como trabajador dependiente, ¿En cuánto tiempo después de egresado consiguió trabajo?</label>
+			</div>
+			<div class="grupo-alternativas">
+				<ul>
+					<li><label for="" class="alternativa" data-pregunta="p12-1" data-indice="a">a) Menos de 1 año.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-1" data-indice="b">b) Entre 1 y 2 años.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-1" data-indice="c">c) Entre 2 y 3 años.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-1" data-indice="d">d) Más de 3 años.</label></li>
+				</ul>
+			</div>
 		</div>
 
-		<div class="pregunta-adicional" id="p7-1">
-			
+		<div class="pregunta-adicional" id="p12-2" data-head="b">
+			<div class="enunciado">
+				<label for="">¿Usted cómo consiguió trabajo de cirujano dentista dependiente?</label>
+			</div>
+			<div class="grupo-alternativas">
+				<ul>
+					<li><label for="" class="alternativa" data-pregunta="p12-2" data-indice="a">a) Recomendaciones Personales.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-2" data-indice="b">b) Acceso por concurso.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-2" data-indice="c">c) Avisos Publicitarios.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-2" data-indice="d">d) Otros.</label></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="pregunta-adicional" id="p12-3" data-head="b">
+			<div class="enunciado">
+				<label for="">¿Cuál fue el grado de dificultad para conseguir empleo como dependiente?</label>
+			</div>
+			<div class="grupo-alternativas">
+				<ul>
+					<li><label for="" class="alternativa" data-pregunta="p12-3" data-indice="a">a) Mucha Dificultad.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-3" data-indice="b">b) Alguna Dificultad.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-3" data-indice="c">c) Ninguna Dificultad.</label></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="pregunta-adicional" id="p12-4" data-head="a">
+			<div class="enunciado">
+				<label for="">Como trabajador independiente, ¿En cuánto tiempo comenzó a tener ingresos?</label>
+			</div>
+			<div class="grupo-alternativas">
+				<ul>
+					<li><label for="" class="alternativa" data-pregunta="p12-4" data-indice="a">a) Menos de 1 año.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-4" data-indice="b">b) Entre 1 y 2 años.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-4" data-indice="c">c) Entre 2 y 3 años.</label></li>
+					<li><label for="" class="alternativa" data-pregunta="p12-4" data-indice="d">d) Más de 3 años.</label></li>
+				</ul>
+			</div>
 		</div>
 	</div>
-	<div class="pregunta" id="p14">
-		<div class="enunciado">
-			<label for="">14. Como trabajador independiente ...</label>
-		</div>
-		<div class="grupo-alternativas">
-			<ul>
-				<li><label for="" class="alternativa" data-pregunta="p1" data-indice="a">a) Independiente.</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p1" data-indice="b">b) Dependiente.</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p1" data-indice="b">c) Dependiente e independiente.</label></li>
-				<li><label for="" class="alternativa" data-pregunta="p1" data-indice="b">d) Desempleado.</label></li>
-			</ul>
-		</div>
-	</div>
-	<div class="pregunta" id="p15">
+	<div class="pregunta-alternativa-simple" id="p15">
 		<div class="enunciado">
 			<label for="">15. ¿Dónde es el medio en el que desarrolla la profesión?</label>
 		</div>
@@ -243,7 +270,7 @@
 			</ul>
 		</div>
 	</div>
-	<div class="pregunta" id="p16">
+	<div class="pregunta-respuesta-tipeada" id="p16">
 		<div class="enunciado">
 			<label for="">16. En la actualidad ¿Cuántas horas al día dedica al ejercicio de la profesion?</label>
 		</div>
@@ -251,15 +278,15 @@
 			<input type="number">
 		</div>
 	</div>
-	<div class="pregunta" id="p17">
+	<div class="pregunta-respuesta-tipeada" id="p17">
 		<div class="enunciado">
-			<label for="">17. ¿Cuál es uningreso medio recibido mensualmente como cirujano dentista?</label>
+			<label for="">17. ¿Cuál es su ingreso medio recibido mensualmente como cirujano dentista?</label>
 		</div>
 		<div class="respuesta-tipeada">
 			<input type="number">
 		</div>
 	</div>
-	<div class="pregunta" id="p18">
+	<div class="pregunta-alternativa-simple" id="p18">
 		<div class="enunciado">
 			<label for="">18.¿Cuál es el grado de satisfacción con el ingreso medio recibido como cirujano dentista?</label>
 		</div>
@@ -271,6 +298,7 @@
 			</ul>
 		</div>
 	</div>
+
 	<button id="boton">Ver Respuestas</button>
 	<div><p>Las respuestas marcadas se ven en la consola del navegador</p></div>
 	<div id="resultado"></div>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,70 @@
 $(document).ready(function () {
 
+	//Caso 1: Pregunta alternativa simple
+	$('.pregunta-alternativa-simple .alternativa').click(function(){
+		var numeroPregunta = $(this).data('pregunta'); //se obtiene numero de pregunta
+		$("#"+numeroPregunta+" .grupo-alternativas li label").each(function(){ 	
+			$(this).attr("data-seleccionado","false");	//se cambia de color las alternativas						
+			$(this).css("color","gray");				//se cambia atributo data-seleccionado -> true
+		});
+		$(this).css('color','red'); 					//se cambia color la alternativa seleccionada
+		$(this).attr("data-seleccionado","true"); 		// se cambia atributo data-seleccionado -> true
+	});
+
+	//Caso ´2: Pregunta alternativa compuesta
+	$('.pregunta-alternativa-multiple .alternativa').on( "click", function(){
+
+		var seleccionado = $(this).data('seleccionado');
+
+		if(seleccionado == false){
+			$(this).attr("data-seleccionado","true");		// no funciona para cambiar internamente, solo de forma visual
+			$(this).data("seleccionado",true); 			// necesario para cambiar estado del atributo data-seleccionado, este si lo hace de forma interna
+			$(this).css('color','green');
+		}else{
+			$(this).attr("data-seleccionado","false");
+			$(this).data("seleccionado",false);
+			$(this).css('color','black');
+		}
+	});
+
+	//Caso 3: Pregunta Compuesta
+	$('.pregunta-compuesta .alternativa').click(function(){
+		var numeroPregunta = $(this).data('pregunta'); 	//se obtiene numero de pregunta
+		var alternativa = $(this).data('indice');		//se ontiene indice de alternativa (a || b ||c ...)
+
+		$("#"+numeroPregunta+" .grupo-alternativas li label[data-pregunta="+numeroPregunta+"]").each(function(){ 	
+			$(this).attr("data-seleccionado","false");	//se cambia atributo data-seleccionado -> false						
+			$(this).css("color","gray");				//se cambia de color las alternativas
+		});
+
+		$(this).css('color','red'); 					//se cambia color la alternativa seleccionada
+		$(this).attr("data-seleccionado","true"); 		// se cambia atributo data-seleccionado -> true
+
+		//La funcionalidad es la siguiente: en una pregunta compuesta, las de "abajo" dependen de la de "arriba", especificamente de la alternativa que se seleccione.
+		//Si la seleccionada es la letra "a" entonces se deberan mostrar (*) las siguientes preguntas que guarden relacion con esta alternativa
+		//y lo mismo si fuese con la letra "b", "c", etc. 
+		//Sim embargo, suponiendo que se eliga la letra "a" y que de esta dependan otras preguntas, el usuario ve la(s) pregunta(s) ocultas y marca una alternativa,
+		//pero decide luego cambiar la letra "a" inicial por la "b" y que de esta tambien dependen otras preguntas. 
+		//Entonces se debera hacer que las preguntas de la alternativa "a" vuelvan a su estado original y no generar conflictos. (**)
+
+		$("#"+numeroPregunta+" .pregunta-adicional").each(function(){ 		
+			var adicional = $(this).data("head");
+			if(alternativa == adicional){
+				$(this).show();	//(*)
+			}else{
+				var numeroPreguntaAdicional = $(this).attr('id');	// (**)
+				$("#"+numeroPreguntaAdicional+" .grupo-alternativas li label[data-pregunta="+numeroPreguntaAdicional+"]").each(function(){ 		
+					$(this).attr("data-seleccionado","false");					
+					$(this).css("color","black");				
+				});
+				$(this).hide();
+			}
+		});
+	});
+
+	//Caso 4: Pregunta con respuesta tipeada
+
+	/*
 	$('.pregunta .alternativa').click(function(){
 		var numeroPregunta = $(this).data('pregunta');
 		$("#"+numeroPregunta+" .grupo-alternativas li label").each(function(){
@@ -19,8 +84,8 @@ $(document).ready(function () {
 			$("#"+numeroPregunta+" .pregunta-adicional").hide();
 		}
 
-	});
-
+	});*/
+	/*
 	$('#boton').click(function(){
 		$(".grupo-alternativas li label[data-seleccionado=true]").each(function(){
 			$(this).css("color","blue");
@@ -43,8 +108,6 @@ $(document).ready(function () {
                     }
             });
 		});
-
-
 	});
 
 	$('.pregunta-alternativa-multiple .alternativa').on( "click", function(){
@@ -52,12 +115,14 @@ $(document).ready(function () {
 		var seleccionado = $(this).data('seleccionado');
 
 		if(seleccionado == "no"){
-			$(this).attr("data-seleccionado","si");
+			$(this).attr("data-seleccionado","si");// no funciona para cambiar internamente, solo de forma visual
+			$(this).data("seleccionado","si"); // necesario para cambiar estado del atributo data-seleccionado, este si lo hace de forma interna
 			$(this).css('color','green');
-		}
-		if(seleccionado == "si"){
+		}else{
 			$(this).attr("data-seleccionado","no");
+			$(this).data("seleccionado","no");
 			$(this).css('color','black');
 		}
-	});
+	});/*
+	*/
 });
